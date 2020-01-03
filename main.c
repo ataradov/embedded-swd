8d38dee5fcaaa1839a91d54112a92e5751f48b58
ooxi <violetland@mail.ru>
2020-01-03 19:13:26 +0100

    Fix UART baudrate
    The UART baudrate setup did not work as expected when using an 8 MHz clock. Setting the baudrate to 57600 would result in an actual baudrate of 9600.
    
    In order to fix this behaviour, I'm changing the internal clock to 48 MHz using the free-dap's samd21 system initialization routine. This also significantly increases the read and write speed of SWD transfers.

--- main.c
+++ main.c
@@ -33,6 +33,7 @@
 #include <string.h>
 #include "samd21.h"
 #include "hal_gpio.h"
+#include "nvm_data.h"
 #include "dap.h"
 
 //-----------------------------------------------------------------------------
@@ -101,8 +102,33 @@ void uart_puthex(uint32_t v, int size)
 //-----------------------------------------------------------------------------
 static void sys_init(void)
 {
-  // Switch to 8MHz clock (disable prescaler)
+  uint32_t coarse, fine;
+
   SYSCTRL->OSC8M.bit.PRESC = 0;
+
+  SYSCTRL->INTFLAG.reg = SYSCTRL_INTFLAG_BOD33RDY | SYSCTRL_INTFLAG_BOD33DET |
+      SYSCTRL_INTFLAG_DFLLRDY;
+
+  NVMCTRL->CTRLB.bit.RWS = 2;
+
+  coarse = NVM_READ_CAL(NVM_DFLL48M_COARSE_CAL);
+  fine = NVM_READ_CAL(NVM_DFLL48M_FINE_CAL);
+
+  SYSCTRL->DFLLCTRL.reg = 0; // See Errata 9905
+  while (0 == (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
+
+  SYSCTRL->DFLLMUL.reg = SYSCTRL_DFLLMUL_MUL(48000);
+  SYSCTRL->DFLLVAL.reg = SYSCTRL_DFLLVAL_COARSE(coarse) | SYSCTRL_DFLLVAL_FINE(fine);
+
+  SYSCTRL->DFLLCTRL.reg = SYSCTRL_DFLLCTRL_ENABLE | SYSCTRL_DFLLCTRL_USBCRM |
+      SYSCTRL_DFLLCTRL_MODE | SYSCTRL_DFLLCTRL_BPLCKC | SYSCTRL_DFLLCTRL_CCDIS |
+      SYSCTRL_DFLLCTRL_STABLE;
+
+  while (0 == (SYSCTRL->PCLKSR.reg & SYSCTRL_PCLKSR_DFLLRDY));
+
+  GCLK->GENCTRL.reg = GCLK_GENCTRL_ID(0) | GCLK_GENCTRL_SRC(GCLK_SOURCE_DFLL48M) |
+      GCLK_GENCTRL_RUNSTDBY | GCLK_GENCTRL_GENEN;
+  while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
 }
 
 //-----------------------------------------------------------------------------
